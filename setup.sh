8563ed425f2795859cd32007e1d95074affff96b
Chris Mann <chrismwann@gmail.com>
2023-05-28 18:15:51 +0100

    Rename setup script

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ done
 ln -sf "$PWD/nvim" ~/.config/nvim
 ln -sf "$PWD/nnvim/lua" ~/.config/nvim/lua
 ln -sf "$PWD/nnvim/lua/plugins" ~/.config/nvim/lua/plugins
+
+ln -sf "$PWD/tokyo_night.itermcolours" ~/.local/share/
