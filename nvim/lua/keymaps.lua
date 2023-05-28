c8fbac7df619f9cc0dce212be152906eb8f365d0
Chris Mann <chrismwann@gmail.com>
2023-05-28 22:34:17 +0100

    Add trouble and update nvim-tree setup

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -81,3 +81,11 @@ keymap({"n", "i", "v"}, "<leader>tt", ":ToggleTerm size=40 dir=~/Desktop directi
 keymap("n", "<leader>s", ":leap_s<CR>", opts )
 keymap("n", "<leader>S", ":leap_S<CR>", opts )
 
+-- trouble
+-- Lua
+keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
+keymap("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts)
+keymap("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", opts)
+keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", opts)
+keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", opts)
+keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", opts)
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -14,6 +14,7 @@ function M.config()
       enable = true,
       update_cwd = true,
     },
+    modifiable  = true,
     renderer = {
       icons = {
         glyphs = {
