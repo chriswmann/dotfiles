92729fb5968f3d2a141bac7c42d40489a909a678
Christopher Mann <cmann@overstoneassociates.com>
2023-05-30 10:15:35 +0100

    Fix issues with links and setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ for file in *; do
 done
 
 ln -sf "$PWD/nvim" ~/.config/nvim
-ln -sf "$PWD/nnvim/lua" ~/.config/nvim/lua
-ln -sf "$PWD/nnvim/lua/plugins" ~/.config/nvim/lua/plugins
+# ln -sf "$PWD/nnvim/lua" ~/.config/nvim/lua
+# ln -sf "$PWD/nnvim/lua/plugins" ~/.config/nvim/lua/plugins
 
 ln -sf "$PWD/tokyo_night.itermcolours" ~/.local/share/
 
