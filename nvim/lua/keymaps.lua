cd663289b67f45f5667dcd7c3102ba18281a86f8
Christopher Mann <cmann@overstoneassociates.com>
2023-05-30 17:32:24 +0100

    Remove insert mode for terminal toggle keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -78,7 +78,7 @@ keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opt
 keymap("n", "<leader>e", ":NvimTreeFindFileToggle!<CR>", opts)
 
 -- toggleterm
-keymap({"n", "i", "v"}, "<leader>tt", ":ToggleTerm size=20 direction=horizontal<CR>", opts)
+keymap({"n", "v"}, "<leader>tt", ":ToggleTerm size=20 direction=horizontal<CR>", opts)
 
 -- leap
 keymap("n", "<leader>s", ":leap_s<CR>", opts )
