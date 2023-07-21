7798cf8a3b5c17599494fa323a3643fdb2c737d1
Chris <cmann@overstoneassociates.com>
2023-07-21 12:01:46 +0100

    Update awesomewm config and theme

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -97,7 +97,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "home/chris/pictures/gwenydd-pexels-lukas-hartmann-1462011.png"
+theme.wallpaper = "/home/chris/pictures/gwenydd-pexels-lukas-hartmann-1462011.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
